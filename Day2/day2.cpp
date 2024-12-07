ccf597d6b396a1337f1613737bcaa266e70856ba
KayGJay <kylegjones13@gmail.com>
2024-12-07 10:54:47 -0600

    Finally finished part 2 with reddit help omg

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -5,23 +5,32 @@
 #include <cmath>
 #include <algorithm>
 
+/* {{7, 6, 4, 2, 1}, {1, 2, 7, 8, 9}, {9, 7, 6, 2, 1},
+                                          {1, 3, 2, 4, 5}, {8, 6, 4, 4, 1}, {1, 3, 6, 7, 9}}; */
+
+
+//Didn't implement til part 2
+bool isSafe(std::vector<int>);
+bool isSafe2(std::vector<int>);
+
 int main(){
     std::ifstream infile("input.txt");
     std::vector<std::vector<int>> nums;
-
+    int inpNum;
     while (infile.peek() != EOF){
         std::vector<int> temp;
         std::string lineStr;
-        getline(infile, lineStr);
+        getline(infile, lineStr, '\n');
         std::istringstream line(lineStr);
-        int num = 0;
-        while (line >> num)
-            temp.push_back(num);
+        while (line >> inpNum && line.peek() != 'n'){
+            temp.push_back(inpNum);
+        }
         nums.push_back(temp);
-    }
+}
+        
 
-    int sum = 0;
-    int unsafe = 0;
+
+    int sum = 0; 
     for (std::vector list : nums){
         std::string inc_dec;
         std::string temp_inc_dec;
@@ -60,7 +69,44 @@ int main(){
     std::cout << sum << std::endl;
 
     //End part1
+    sum = 0;
+    bool second_pass = false;
+    for (std::vector<int>& list : nums){
+        if (isSafe2(list))
+            sum++;
+    }
 
-    
-    return 0;
+    std::cout << sum << std::endl;
+}
+
+bool isSafe(std::vector<int> vec){
+    bool inc = true;
+    bool dec = true;
+    for (std::vector<int>::size_type i = 1; i < vec.size(); i++){
+        int dif = vec.at(i-1) - vec.at(i);
+        if (dif < -3 || dif > 3 || dif == 0)
+            return false;
+        else{
+            if (dif > 0)
+                inc = false;
+            else
+                dec = false;
+        }
+    }
+    return inc || dec;
+}
+
+bool isSafe2(std::vector<int> vec){
+    if (isSafe(vec))
+        return true;
+    for (std::vector<int>::size_type i = 0; i < vec.size(); i++){
+        std::vector<int> skipper;
+        for (std::vector<int>::size_type j = 0; j < vec.size(); j++){
+            if (i != j)
+                skipper.push_back(vec.at(j));
+        }
+        if (isSafe(skipper))
+            return true;
+    }
+    return false;
 }
